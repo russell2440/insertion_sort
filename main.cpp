827e253225bc997f24b4ab3f4c3f70fde9a45fe5
russell2440 <85845760+russell2440@users.noreply.github.com>
2021-10-03 23:20:03 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+const unsigned int numbers[] = {99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
+
+void insertion_sort(unsigned int input[]) {
+  
+}
+
+insertion_sort(numbers);
+//console.log(numbers);
+
+int main() {
+  std::cout << "Hello World!\n";
+}
