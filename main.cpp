0408e75bc300ed5f54ca8b986939fce373b1a146
russell2440 <85845760+russell2440@users.noreply.github.com>
2021-10-15 01:14:37 +0000

    update

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <list>
+//#include "~/doublelinkedlist/double_linked_list.h"
 
-const unsigned int numbers[] = {99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
+const int numbers[] = {99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
 
-void insertion_sort(unsigned int input[]) {
+void insertion_sort(const int *pinput, const int *pend){
+  //double_linked_list dll(input,length);
+  std::list<int> input(pinput, pend);
+  for (std::list<int>::iterator it = input.begin(); it != input.end(); it++){
+    printf("%d ",*it);
+  }
   
 }
 
-insertion_sort(numbers);
 //console.log(numbers);
 
 int main() {
+  void insertion_sort(const int *pinput, const int *pend);
   std::cout << "Hello World!\n";
+  insertion_sort(numbers, numbers+(sizeof(numbers)/sizeof(*numbers)));
+
 }
