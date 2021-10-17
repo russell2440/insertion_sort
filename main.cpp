cbbd5807aad264caab8c9a5b0942fd2e22302cfd
russell2440 <85845760+russell2440@users.noreply.github.com>
2021-10-17 01:18:32 +0000

    Done.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #include <list>
 
 
-void insertion_sort(const int *pinput, const int *pend){
+void insertion_sort(const int *pinput, const int *pend, int *poutput){
+
   // Construct list from input
   std::list<int> input(pinput, pend);
 
@@ -12,25 +13,34 @@ void insertion_sort(const int *pinput, const int *pend){
     // search loop
     for (std::list<int>::iterator sit = input.begin(); sit != cwit; sit++){
       if(*cwit < *sit){
+        // swap lesser in
         input.splice(sit, input, cwit);   
         break;
       }
     }
   }
 
-  for (std::list<int>::iterator cwit = input.begin(); cwit != input.end(); cwit++){
-    printf(" %d",*cwit);
+  // Store sorted list into output
+  for (std::list<int>::iterator it = input.begin(); it != input.end(); it++){
+    *(poutput++) = *it;
   }
-  
 }
 
-//console.log(numbers);
-
 int main() {
-  const int numbers[] = {99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
-  void insertion_sort(const int *pinput, const int *pend);
+  int numbers[] = {99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
+  void insertion_sort(const int *pinput, const int *pend, int *poutput);
 
   std::cout << "Hello Russ! insert_sort() test\n";
 
-  insertion_sort(numbers, numbers+(sizeof(numbers)/sizeof(*numbers)));
+  for (int *p = numbers; p < numbers+(sizeof(numbers)/sizeof(*numbers)); p++){
+    printf(" %d", *p);
+  }
+  printf("\n");
+
+  insertion_sort(numbers, numbers+(sizeof(numbers)/sizeof(*numbers)), numbers);
+
+  for (int *p = numbers; p < numbers+(sizeof(numbers)/sizeof(*numbers)); p++){
+    printf(" %d", *p);
+  }
+  printf("\n");
 }
