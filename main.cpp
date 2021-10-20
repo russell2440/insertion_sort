655d50d2cabef3c17db4963616712ecd0a46c0a1
russell2440 <85845760+russell2440@users.noreply.github.com>
2021-10-20 03:31:41 +0000

    ok, really done

--- main.cpp
+++ main.cpp
@@ -2,21 +2,52 @@
 #include <vector>
 #include <list>
 
+void display_list(std::list<int> &l, std::list<int>::iterator &cwit){
+  for(std::list<int>::iterator it = l.begin(); it != l.end(); it++){
+    if(cwit == it){
+      printf(" (%d)", *it);
+    }else{
+      printf(" %d", *it);  
+    }
+  }
+  printf("\n");
+}
 
+void display_list(std::list<int> &l){
+  for(std::list<int>::iterator it = l.begin(); it != l.end(); it++){
+      printf(" %d", *it);  
+  }
+  printf("\n");
+}
+
+//
+// Insertion sort:
+// Using std list, initializing with pointers to input array
+// ends and output array start.
+// Algorithm uses outer loop for the current working element
+// and innter loop for search/insertion loop.
+//
 void insertion_sort(const int *pinput, const int *pend, int *poutput){
 
   // Construct list from input
   std::list<int> input(pinput, pend);
 
   // current working element loop
-  for (std::list<int>::iterator cwit = input.begin(); cwit != input.end(); cwit++){
-    // search loop
+  for (std::list<int>::iterator cwit = input.begin(); cwit != input.end();){
+    // Insertion loop
+    bool spliced = false;
     for (std::list<int>::iterator sit = input.begin(); sit != cwit; sit++){
       if(*cwit < *sit){
-        // swap lesser in
-        input.splice(sit, input, cwit);   
+        // Keep cw iteration continuity
+        std::list<int>::iterator tmpit =  cwit; cwit++;
+        // Transfer lesser in
+        input.splice(sit, input, tmpit);
+        spliced = true;   
         break;
       }
+    }
+    if(!spliced){
+      cwit++;
     }
   }
 
@@ -26,20 +57,23 @@ void insertion_sort(const int *pinput, const int *pend, int *poutput){
   }
 }
 
+
 int main() {
   int numbers[] = {99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0};
-  void insertion_sort(const int *pinput, const int *pend, int *poutput);
+  unsigned int numbers_len = (sizeof(numbers)/sizeof(*numbers));
 
   std::cout << "Hello Russ! insert_sort() test\n";
 
-  for (int *p = numbers; p < numbers+(sizeof(numbers)/sizeof(*numbers)); p++){
+  for (int *p = numbers; p < numbers+numbers_len; p++){
     printf(" %d", *p);
   }
+  printf("\n\n");
+
+  ::insertion_sort(numbers, numbers+numbers_len, numbers);
+
   printf("\n");
 
-  insertion_sort(numbers, numbers+(sizeof(numbers)/sizeof(*numbers)), numbers);
-
-  for (int *p = numbers; p < numbers+(sizeof(numbers)/sizeof(*numbers)); p++){
+  for (int *p = numbers; p < numbers+numbers_len; p++){
     printf(" %d", *p);
   }
   printf("\n");
